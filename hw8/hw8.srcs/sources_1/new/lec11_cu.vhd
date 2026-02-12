48d6b7ef8d1e85340426f11cf3c16237aaa46c9d
brugraz <122693180+brugraz@users.noreply.github.com>
2026-02-12 16:28:18 -0700

    lab 2 2.12

--- hw8/hw8.srcs/sources_1/new/lec11_cu.vhd
+++ hw8/hw8.srcs/sources_1/new/lec11_cu.vhd
@@ -36,15 +36,15 @@ entity lec11_cu is
            reset : in STD_LOGIC;
            kbClk : in STD_LOGIC;
            sw : in STD_LOGIC;
-           cw : in STD_LOGIC_VECTOR (3 downto 0);
-           busy : in STD_LOGIC);
+           cw : out STD_LOGIC_VECTOR (3 downto 0);
+           busy : out STD_LOGIC);
 end lec11_cu;
 
 architecture Behavioral of lec11_cu is
 
-signal cw : std_logic_vector(3 downto 0);
-signal sw : std_logic;
-type state_t is (waitstart, loopcond, gathershift, scanout);
+--signal w_cw : std_logic_vector(3 downto 0);
+--signal w_sw : std_logic;
+type state_t is (waitstart, whilehi_wait, read_in_serial, whilelow_wait, send_parallel);
 
 signal state : state_t := waitstart;
 
@@ -57,21 +57,32 @@ if rising_edge(clk) then
 if reset = '0' then state <= waitstart; end if;
 case state is
   when waitstart =>
-    if kbClk = '0' then state <= loopcond; end if;
-  when loopcond =>
-    if kbClk = '0' then state <= gathershift; end if;
-  when gathershift =>
-    if kbClk = '0' then state <= gathershift;
-    elsif sw = '0' then state <= loopcond;
-    elsif sw = '1' then state <= scanout;
-    end if;
-  when scanout =>
-    if busy = '0' then state <= waitstart; end if;
+    if kbClk = '0' then state <= whilehi_wait; end if;
+    -- elsif = '1' then stay
+    -- sw dont care
+  when whilehi_wait =>
+    if kbClk = '0' then state <= read_in_serial; end if;
+  when read_in_serial => 
+    -- kbClk should always be LOW by here
+    state <= whilelow_wait;
+    -- sw don't care
+  when whilelow_wait =>
+    if kbClk = '0' then state <= whilelow_wait;
+    elsif sw = '0' then state <= whilehi_wait;
+    elsif sw = '1' then state <= send_parallel; end if;
+  when send_parallel =>
+    state <= waitstart;
 end case;
 end if;
 
 end process;
 
 -- assign outputs to states
+cw <= "0011" when state = waitstart else
+      "0000" when state = whilehi_wait else
+      "0101" when state = read_in_serial  else
+      "0000" when state = whilelow_wait else
+      "1000" when state = send_parallel;
+busy <= '0' when state = waitstart else '1';
 
 end Behavioral;
---
+++ lab2/lab2.srcs/sources_1/imports/lab2/vga_signal_generator.vhd
@@ -0,0 +1,111 @@
+-- vga_signal_generator Generates the hsync, vsync, blank, and row, col for the VGA signal
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.ece383_pkg.ALL;
+
+entity vga_signal_generator is
+Port ( clk : in STD_LOGIC;
+       reset_n : in STD_LOGIC;
+       position: out coordinate_t;
+       vga : out vga_t);
+end vga_signal_generator;
+
+architecture vga_signal_generator_arch of vga_signal_generator is
+
+  signal horizontal_roll, vertical_roll: std_logic := '0';		
+  signal h_counter_ctrl, v_counter_ctrl: std_logic := '1'; -- Default to counting up
+  signal h_sync_is_low, v_sync_is_low, h_blank_is_low, v_blank_is_low : boolean := false;
+  signal current_pos : coordinate_t;
+    
+  constant h_max_px_total  : integer := 800;
+  constant v_max_px_total  : integer := 525;
+  constant px_num_bits     : integer := 10;
+
+  constant h_max_px_visual : integer := 640;
+  constant v_max_px_visual : integer := 480;
+
+  constant h_front_porch_pxw : integer := 16;
+  constant h_sync_pxw        : integer := 96;
+  constant h_back_porch_pxw  : integer := 48;
+
+  constant v_front_porch_pxw : integer := 10;
+  constant v_sync_pxw        : integer := 2;
+  constant v_back_porch_pxw  : integer := 33;
+  -- component counter was declared in ece383_pkg that is used .ALL
+    
+begin
+-- horizontal counter
+h_counter : counter
+generic map(
+  num_bits  => px_num_bits,
+  max_value => h_max_px_total-1
+)
+port map(
+  clk     => clk,
+  ctrl    => h_counter_ctrl,
+  reset_n => reset_n,
+  roll    => horizontal_roll,
+  Q       => current_pos.col
+);
+    
+-- Glue code to connect the horizontal and vertical counters
+v_counter_ctrl <= horizontal_roll;
+
+-- vertical counter
+  v_counter : counter
+  generic map(
+    num_bits  => px_num_bits,
+    max_value => v_max_px_total-1
+  )
+  port map(
+    clk     => clk,
+    ctrl    => v_counter_ctrl,
+    reset_n => reset_n,
+    roll    => vertical_roll,
+    Q       => current_pos.row
+  );
+  
+-- Assign VGA outputs in a gated manner
+
+process (clk)
+begin
+  if (rising_edge(clk)) then
+  -- could we have used <= ... when ... here
+    if current_pos.col >= (h_max_px_visual-1) and not (current_pos.col = h_max_px_total-1) then
+      h_blank_is_low <= false; else h_blank_is_low <= true; end if;
+      
+    if current_pos.col >= (h_max_px_visual-1+h_front_porch_pxw)
+   and current_pos.col < (h_max_px_visual-1+h_front_porch_pxw+h_sync_pxw) then
+      h_sync_is_low  <= true;  else h_sync_is_low <= false; end if;
+
+    if current_pos.row < v_max_px_visual then
+      v_blank_is_low <= true; else v_blank_is_low <= false; end if;
+      
+    if current_pos.row >= (v_max_px_visual+v_front_porch_pxw)
+   and current_pos.row < (v_max_px_visual+v_front_porch_pxw+v_sync_pxw) then
+      v_sync_is_low  <= true;  else v_sync_is_low <= false; end if;
+      
+    -- cases on edge of vsync/blank rise and fall (@ column 799->000)
+    if current_pos.col = h_max_px_total-1 then
+      if current_pos.row = v_max_px_visual-1 then v_blank_is_low <= false; end if;
+      
+      if current_pos.row = v_max_px_total-1 then v_blank_is_low <= true; end if;
+        
+      if current_pos.row = v_max_px_visual-1+v_front_porch_pxw
+      then v_sync_is_low <= true; end if;
+        
+      if current_pos.row = v_max_px_visual-1+v_front_porch_pxw+v_sync_pxw
+      then v_sync_is_low <= false; end if;
+    end if;
+  end if;
+end process;
+
+-- Assign output ports
+position <= current_pos;
+vga.hsync <= '1' when not h_sync_is_low else '0';
+vga.vsync <= '1' when not v_sync_is_low else '0';
+vga.blank <= '1' when ((not h_blank_is_low) or (not v_blank_is_low)) else '0';
+
+end vga_signal_generator_arch;
